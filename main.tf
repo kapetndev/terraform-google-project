b680e3290e1aa54e2b4b425783689681bbe41e61
Tomas Basham <tomasbasham@gmail.com>
2023-08-08 22:04:26 +0100

    feat: add Google Cloud Platform project module

---
+++ main.tf
@@ -0,0 +1,34 @@
+locals {
+  parent_type = var.parent != null ? split("/", var.parent)[0] : null
+  parent_id   = var.parent != null ? split("/", var.parent)[1] : null
+  prefix      = var.prefix != null ? "${var.prefix}-" : ""
+  project_id  = var.project_id != null ? var.project_id : random_id.project_id.hex
+}
+
+resource "random_id" "project_id" {
+  byte_length = 4
+  prefix      = "${local.prefix}${var.name}-"
+}
+
+resource "google_project" "project" {
+  auto_create_network = var.auto_create_network
+  billing_account     = var.billing_account
+  folder_id           = local.parent_type == "folders" ? local.parent_id : null
+  name                = coalesce(var.descriptive_name, "${local.prefix}${var.name}")
+  org_id              = local.parent_type == "organizations" ? local.parent_id : null
+  project_id          = local.project_id
+}
+
+resource "google_project_service" "services" {
+  for_each                   = var.services
+  disable_dependent_services = var.disable_dependent_services
+  disable_on_destroy         = var.disable_on_destroy
+  project                    = google_project.project.project_id
+  service                    = each.key
+}
+
+resource "google_tags_tag_binding" "binding" {
+  for_each  = var.tag_bindings
+  parent    = "//cloudresourcemanager.googleapis.com/projects/${google_project.project.number}"
+  tag_value = each.value
+}
