5df9d157832b85350a9a1249963120e7cd9fbe20
Tomas Basham <tomasbasham@gmail.com>
2023-08-08 22:04:52 +0100

    docs(project): add example usage

---
+++ examples/services-iam/terraform.auto.tfvars
@@ -0,0 +1,2 @@
+billing_account = "000000-ABCDEF-123456"
+organization_id = "1000000000000"
---
+++ examples/services-iam/variables.tf
@@ -0,0 +1,9 @@
+variable "billing_account" {
+  description = "The alphanumeric ID of the billing account this project belongs to. "
+  type        = string
+}
+
+variable "organization_id" {
+  description = "The numeric ID of the organization this project belongs to."
+  type        = string
+}
